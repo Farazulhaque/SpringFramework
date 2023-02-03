0c2536719e58c0cac13d26bb947d8060690e6b6a
Farazulhaque <farazattari786@gmail.com>
2023-02-03 11:01:15 +0530

    constructor injection

--- spring-demo-one/src/com/luv2code/springdemo/MyApp.java
+++ spring-demo-one/src/com/luv2code/springdemo/MyApp.java
@@ -3,11 +3,12 @@ package com.luv2code.springdemo;
 public class MyApp {
 
 	public static void main(String[] args) {
+
 		// create the object
-		Coach coach = new TrackCoach();
+		Coach theCoach = new TrackCoach();
 
 		// use the object
-		System.out.println(coach.getDailyWorkout());
+		System.out.println(theCoach.getDailyWorkout());
 	}
 
 }
